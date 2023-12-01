3aa1777b5e2b5471a196baf46f581be0b978b902
Joseph <jtewold1@msudenver.edu>
2023-11-30 23:18:06 -0700

    Commit #3 - Finished Implementing User Story #2 for viewing playlists created. Also, improved on the appearance of the html templates by adding a navigation bar to replace some buttons

--- src/app/routes.py
+++ src/app/routes.py
@@ -18,7 +18,7 @@ import bcrypt
 @app.route('/')
 @app.route('/index')
 @app.route('/index.html')
-def index():
+def show_index():
     """ This function is meant for implementing the index route for the application"""
     return render_template('index.html')
 
@@ -82,7 +82,7 @@ def users_signout():
     """This route handles the signout process, logging out the user and redirecting the user to the index page."""
     logout_user()
     flash('You were successfully logged out!')
-    return redirect(url_for('index'))
+    return redirect(url_for('show_index'))
 
 # TODO 3: Implement User Story 2: View Playlists (Refer to one of the previous homeworks when viewing all of the invoices for this)
 
@@ -91,7 +91,9 @@ def users_signout():
 @login_required
 def view_playlists():
     """ This function is meant for implementing the view playlists route for the application """
-    return render_template('playlists.html', title='Playlists')
+
+    playlists = Playlist.query.filter_by(users_id=current_user.id).all() # Get all of the playlists from the database
+    return render_template('playlists.html', title='Playlists', playlists=playlists) # Render the playlists.html template and pass the playlists to it
 
 # TODO 4: Implement User Story 3: Create a Playlist (Refer to one of the previous homeworks when creating an invoice for this)
 
@@ -116,17 +118,17 @@ def view_playlists_videos(playlist_id):
 
 @app.route('/playlists/<playlist_id>/videos/add', methods=['GET', 'POST'])
 @login_required
-def add_playlists_videos(playlist_id):
+def add_playlists_video(playlist_id):
     """ This function is meant for implementing the add videos to playlist route for the application """
     pass
 
 # TODO 7: Implement User Story 5: Deleting videos from a playlist(Refer to Homework 3 when deleting an invoice item for this)
 
 
-@app.route('/playlists/<playlist_id>/videos/delete', methods=['GET', 'POST'])
+@app.route('/playlists/<playlist_id>/<video_id>/delete', methods=['GET', 'POST'])
 @login_required
-def delete_playlists_videos(playlist_id):
-    """ This function is meant for implementing the delete videos from playlist route for the application """
+def delete_playlists_video(video_id):
+    """ This function is meant for implementing the delete a video from playlist route for the application """
     pass
 
 # TODO 8: Implement User Story 6: Deleting a playlist (Refer to Homework 3 when deleting an invoice for this)
